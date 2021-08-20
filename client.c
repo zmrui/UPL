ad281335305670d236743a2ce19236acefd75a21
zmrui <zmrui>
2021-08-20 14:25:04 -0500

    202108201425

--- client.c
+++ client.c
@@ -6,48 +6,30 @@
 #include <sys/types.h>  //struct used in socket
 #include <netinet/in.h> //Linux IPv4 protocol implementation
 #include <arpa/inet.h>  //convert IP address from text to binary
-#include <time.h>
+#include <sys/time.h>
 
 FILE fp;
 int save_one_log(int cnt, clock_t UPL){
 
 }
-/*
-int MySend(int fd, int Bytes){
-    if(Bytes>0){
-    char WBuffer1 = "11111111";
-    char WBuffer2 = "cltend==";
-    int cnt;
-    for (cnt=0;cnt<Bytes-1;cnt++){
-        write(fd,WBuffer1,strlen(WBuffer1));
-    }
-    write(fd,WBuffer2,strlen(WBuffer1));
-    }
-}
-
-int MyReceive(int fd){
-    char RBuffer [1024];
-    memset(RBuffer,0,sizeof(RBuffer));
-    while(read())
-
-}*/
 
 int main(int argc, char** argv)
 {
     char* IPADDR = malloc(sizeof(char)*128);
     int PORT;
     int COUNT;
-    int SIZE;
-    clock_t StartClock, EndClock, UPLTime;
-    if (argc != 5 || strlen(argv[1]>30) ){
-        printf("client [IP ADDRESS] [PORT] [COUNT] [SIZE]\nExample: ./client 10.10.10.1 80 2 1024\n");
+    int SIZEc2s, SIZEs2c;
+    struct timeval StartTime, EndTime;
+    if (argc != 6 ){
+        printf("client [IP ADDRESS] [PORT] [COUNT] [SIZEc2s] [SIZEs2c] \nExample: ./client 10.10.10.1 80 2 1024 2048\n");
         return 0;
     }
     else{
         strcpy(IPADDR,argv[1]);
         PORT = atoi(argv[2]);
         COUNT = atoi(argv[3]);
-        SIZE = atoi(argv[4]);
+        SIZEc2s = atoi(argv[4]);
+        SIZEs2c = atoi(argv[5);
     }
 
     struct sockaddr_in SERVER;
@@ -58,33 +40,27 @@ int main(int argc, char** argv)
     SERVER.sin_port = PORT;
     SERVER.sin_family = AF_INET;
 
-    char WBuffer[8], RBuffer[1024]; memset(WBuffer, 1, sizeof(WBuffer));
+    char WBuffer[102400], RBuffer[102400];
+
     int MySocket;
+    MySocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(connect(MySocket,(struct sockaddr*) &SERVER, sizeof(SERVER))<0){
+        printf("[CLIENT]: Connect failed.\n");
+        exit(1);
+    }
+
     int cnt = 0;
-    for( cnt = 0; cnt < COUNT; cnt++){
+    for(cnt = 0; cnt < COUNT; cnt++){
+        gettimeofday(&StartTime, NULL);
 
-        StartClock = clock();
+        write(MySocket,WBuffer,SIZEc2s);
 
-        MySocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if(connect(MySocket,(struct sockaddr*) &SERVER, sizeof(SERVER))<0){
-            printf("Connect failed.\n");
-            exit(1);
-        }
+        read (MySocket,RBuffer,SIZEs2c);
 
-        int i;
-        for (i=0;i<SIZE;i++){
-            write(MySocket,WBuffer,strlen(WBuffer));
-        }
-
-        while(read(MySocket,RBuffer,1000)==0){
-            ;
-        }
-
-
-//TODO
-        EndClock = clock();
-        UPLTime = EndClock - StartClock;
-        printf("UPL is %d\n",UPLTime);
+        gettimeofday(&EndTime, NULL);
+        
+        printf("[CLIENT]: Client's [No.%d] UPL Time is [%ld Second, %ld Microsecond].\n", 
+                                    cnt+1, (EndTime.tv_sec-StartTime.tv_sec), (EndTime.tv_usec-StartTime.tv_usec));
     }
     return 0;
 }
--- server.c
+++ server.c
@@ -17,14 +17,15 @@ int save_one_log(int cnt, clock_t UPL){
 int main(int argc, char** argv)
 {
     int PORT;
-    int SIZE;
-    if (argc != 3 || strlen(argv[1]>30) ){
-        printf("server [PORT] [SIZE]\nExample: ./server 80 1024\n");
+    int SIZEc2s, SIZEs2c;
+    if (argc != 4){
+        printf("server [PORT] [SIZEc2s] [SIZEs2c]\nExample: ./server 80 1024 2048\n");
         return 0;
     }
     else{
         PORT = atoi(argv[1]);
-        SIZE = atoi(argv[2]);
+        SIZEc2s = atoi(argv[2]);
+        SIZEs2c = atoi(argv[3]);
     }
 
     struct sockaddr_in SERVER, CLIENT;
@@ -33,32 +34,35 @@ int main(int argc, char** argv)
     SERVER.sin_port = PORT;
     SERVER.sin_family = AF_INET;
 
-    char WBuffer[8], RBuffer[1024]; memset(WBuffer, 1, sizeof(WBuffer));
+    char WBuffer[102400], RBuffer[102400];
+
     int ListenSocket, ResponseSocket;
     socklen_t CLIENT_LEN;
+    ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(bind(ListenSocket, (struct sockaddr*) &SERVER, sizeof(SERVER))<0){
+        printf("[SERVER]: bind failed.\n");
+        exit(1);
+    }
+
+    listen(ListenSocket,5);
+    CLIENT_LEN = sizeof(CLIENT);
+    ResponseSocket=accept(ListenSocket, (struct sockaddr*) &CLIENT, &CLIENT_LEN);
+    if(ResponseSocket>0){
+        printf("[SERVER]: Socket to client established.\n");
+    }
+    else{
+        printf("[SERVER]: Failed to establish socket to client.\n");
+        exit(1);
+    }
+
+
     int cnt = 0;
     while(1){
-        ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if(bind(ListenSocket, (struct sockaddr*) &SERVER, sizeof(SERVER))<0){
-            printf("bind failed.\n");
-            exit(1);
-        }
+        
+        read (ResponseSocket, RBuffer, SIZEc2s);
 
-        listen(ListenSocket,5);
-        CLIENT_LEN = sizeof(CLIENT);
-        ResponseSocket=accept(ListenSocket, (struct sockaddr*) &CLIENT, &CLIENT_LEN);
+        write(ResponseSocket, WBuffer, SIZEs2c);
 
-
-        while(read(ResponseSocket,RBuffer,1000)==0){
-            ;
-        }
-        int i;
-        for (i=0;i<SIZE;i++){
-            write(ResponseSocket, WBuffer, strlen(WBuffer));
-        }
-
-        close(ListenSocket);
-        close(ResponseSocket);
     }
     return 0;
 }
